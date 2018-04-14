a899e94cf870ec626e078d3ea96a90edb17c42e5
Braydon Fuller <courier@braydon.com>
2018-04-13 22:10:26 -0700

    2 to 4 spaces for c

--- binary-deep-copy/c/main.c
+++ binary-deep-copy/c/main.c
@@ -4,74 +4,74 @@
 #include <stdbool.h>
 
 typedef struct bnode {
-  struct bnode *left;
-  struct bnode *right;
-  int value;
+    struct bnode *left;
+    struct bnode *right;
+    int value;
 } bnode_t;
 
 bnode_t *new_bnode(int value)
 {
-  bnode_t *node = calloc(1, sizeof(bnode_t));
-  node->value = value;
-  return node;
+    bnode_t *node = calloc(1, sizeof(bnode_t));
+    node->value = value;
+    return node;
 }
 
 bnode_t *deep_copy(bnode_t *original)
 {
-  bnode_t *new = new_bnode(original->value);
-  if (original->left) {
-    new->left = deep_copy(original->left);
-  }
-  if (original->right) {
-    new->right = deep_copy(original->right);
-  }
-  return new;
+    bnode_t *new = new_bnode(original->value);
+    if (original->left) {
+	new->left = deep_copy(original->left);
+    }
+    if (original->right) {
+	new->right = deep_copy(original->right);
+    }
+    return new;
 }
 
 bnode_t *test_new_btree()
 {
-  bnode_t *node = new_bnode(10);
-  node->left = new_bnode(6);
-  node->right = new_bnode(18);
-  node->left->left = new_bnode(3);
-  node->left->right = new_bnode(7);
-  node->left->left->left = new_bnode(1);
-  return node;
+    bnode_t *node = new_bnode(10);
+    node->left = new_bnode(6);
+    node->right = new_bnode(18);
+    node->left->left = new_bnode(3);
+    node->left->right = new_bnode(7);
+    node->left->left->left = new_bnode(1);
+    return node;
 }
 
 int test_check_btree(bnode_t *node)
 {
-  if (node->value != 10) {
-    return false;
-  }
-  if (node->left->value != 6) {
-    return false;
-  }
-  if (node->right->value != 18) {
-    return false;
-  }
-  if (node->left->left->value != 3) {
-    return false;
-  }
-  if (node->left->right->value != 7) {
-    return false;
-  }
-  if (node->left->left->left->value != 1) {
-    return false;
-  }
-  return true;
+    if (node->value != 10) {
+	return false;
+    }
+    if (node->left->value != 6) {
+	return false;
+    }
+    if (node->right->value != 18) {
+	return false;
+    }
+    if (node->left->left->value != 3) {
+	return false;
+    }
+    if (node->left->right->value != 7) {
+	return false;
+    }
+    if (node->left->left->left->value != 1) {
+	return false;
+    }
+    return true;
 }
 
 int main(int argc, char **argv)
 {
-  bnode_t *node = test_new_btree();
-  bnode_t *copy = deep_copy(node);
+    bnode_t *node = test_new_btree();
+    bnode_t *copy = deep_copy(node);
 
-  if (!test_check_btree(copy)) {
-    printf("failed\n");
-    return 1;
-  } else {
-    printf("passed\n");
-    return 0;
-  }
+    if (!test_check_btree(copy)) {
+	printf("failed\n");
+	return 1;
+    } else {
+	printf("passed\n");
+	return 0;
+    }
 }
--- fibonacci/c/main.c
+++ fibonacci/c/main.c
@@ -9,19 +9,19 @@
 #define RESET "\x1b[0m"
 
 uint64_t fib(int n) {
-  if (n == 0) {
-    return 0;
-  }
-  uint64_t prev = 0;
-  uint64_t next = 1;
+    if (n == 0) {
+	return 0;
+    }
+    uint64_t prev = 0;
+    uint64_t next = 1;
 
-  for (int i = 1; i < n; i++) {
-    uint64_t _prev = prev;
-    prev = next;
-    next = _prev + next;
-  }
+    for (int i = 1; i < n; i++) {
+	uint64_t _prev = prev;
+	prev = next;
+	next = _prev + next;
+    }
 
-  return next;
+    return next;
 }
 
 uint64_t expected[] = {0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229,832040,1346269,2178309,3524578,5702887,9227465,14930352,24157817,39088169,63245986,102334155,165580141,267914296,433494437,701408733,1134903170,1836311903,2971215073,4807526976,7778742049,12586269025};
@@ -29,17 +29,17 @@ int expected_length = 51;
 
 int main(int argc, char **argv) {
 
-  int status = 0;
+    int status = 0;
 
-  for (int i = 0; i < expected_length; i++) {
-    uint64_t result = fib(i);
-    if (result != expected[i]) {
-      printf(RED "failed" RESET " for %i: actual: %"PRIu64", expected: %"PRIu64"\n", i, result, expected[i]);
-      status = 1;
-    } else {
-      printf(GREEN "passed" RESET " for %i, actual: %"PRIu64"\n", i, result);
+    for (int i = 0; i < expected_length; i++) {
+	uint64_t result = fib(i);
+	if (result != expected[i]) {
+	    printf(RED "failed" RESET " for %i: actual: %"PRIu64", expected: %"PRIu64"\n", i, result, expected[i]);
+	    status = 1;
+	} else {
+	    printf(GREEN "passed" RESET " for %i, actual: %"PRIu64"\n", i, result);
+	}
     }
-  }
 
-  return status;
+    return status;
 }
